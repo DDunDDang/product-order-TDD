5ec4bdb7a046ea0202f51d76bef68720e465129f
DDunDDang <thdwns0331@gmail.com>
2024-05-31 16:47:28 +0900

    init

---
+++ src/main/java/org/example/productordertdd/ProductOrderTddApplication.java
@@ -0,0 +1,13 @@
+package org.example.productordertdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderTddApplication.class, args);
+    }
+
+}
