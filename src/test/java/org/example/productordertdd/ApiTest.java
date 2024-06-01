32aebe945fc80b7e73ffd0c155449832f68172d5
DDunDDang <thdwns0331@gmail.com>
2024-06-01 20:49:10 +0900

    API Test

---
+++ src/test/java/org/example/productordertdd/ApiTest.java
@@ -0,0 +1,19 @@
+package org.example.productordertdd;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+}
