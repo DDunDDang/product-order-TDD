fe5ca7ce2201f23da215203f9cd899c78dbb9c13
DDunDDang <thdwns0331@gmail.com>
2024-06-04 19:35:51 +0900

    메서드 분리

--- src/test/java/org/example/productordertdd/product/ProductSteps.java
+++ src/test/java/org/example/productordertdd/product/ProductSteps.java
@@ -23,4 +23,13 @@ public class ProductSteps {
         final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
         return new AddProductRequest(name, price, discountPolicy);
     }
+
+    public static ExtractableResponse<Response> 상품조회요청(Long productId) {
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+        return response;
+    }
 }
