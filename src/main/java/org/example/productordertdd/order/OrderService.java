9bed6d1dd3567ce9fefcb4c4464fe1c5dfed928a
DDunDDang <thdwns0331@gmail.com>
2024-06-08 09:38:36 +0900

    트랜잭션 추가

--- src/main/java/org/example/productordertdd/order/OrderService.java
+++ src/main/java/org/example/productordertdd/order/OrderService.java
@@ -3,6 +3,7 @@ package org.example.productordertdd.order;
 import org.example.productordertdd.product.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ class OrderService {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<Void> createOrder(@RequestBody CreateOrderRequest request) {
         final Product product = orderPort.getProductById(request.productId());
 
