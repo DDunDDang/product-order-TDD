346402521a320a0a569c149ce817321843255184
DDunDDang <thdwns0331@gmail.com>
2024-06-09 17:11:59 +0900

    주문 결제 기능 JPA 연동

--- src/main/java/org/example/productordertdd/payment/Payment.java
+++ src/main/java/org/example/productordertdd/payment/Payment.java
@@ -1,12 +1,24 @@
 package org.example.productordertdd.payment;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.productordertdd.order.Order;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "payments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Order order;
-    private final String cardNumber;
+    @OneToOne
+    private Order order;
+    private String cardNumber;
 
     public Payment(Order order, String cardNumber) {
         this.order = order;
@@ -15,19 +27,8 @@ class Payment {
         Assert.hasText(cardNumber, "카드 번호는 필수입니다.");
     }
 
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
     public int getPrice() {
         return order.getTotalPrice();
     }
 
-    public String getCardNumber() {
-        return cardNumber;
-    }
 }
